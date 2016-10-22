29a2332a32a157c80f3705287cebe0345a804c02
rozbom <75200306@qq.com>
2016-10-22 19:45:04 +0800

    update footer

--- src/components/App.vue
+++ src/components/App.vue
@@ -20,11 +20,11 @@
         <div class="container">
             <div class="content has-text-centered">
                 <p>
-                    <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+                    <strong>simple blog</strong> by <a href="http://post.zz173.com">rozbo</a>.
                 </p>
                 <p>
-                    <a class="icon" href="https://github.com/jgthms/bulma">
+                    say thanks to
+                    <a class="icon" href="https://github.com/rozbo/blog">
                         <i class="fa fa-github"></i>
                     </a>
                 </p>
