63cad24d4de3fa307fe80d41cceec6aa81da9c30
rozbom <75200306@qq.com>
2017-01-02 19:05:43 +0800

    auto publish

--- src/helpers/render.js
+++ src/helpers/render.js
@@ -1,7 +1,7 @@
 import marked from 'marked'
 import Prism from 'prismjs'
 import prism_php from 'prismjs/components/prism-php'
-import prism_shell from 'prismjs/components/prism-shell'
+import prism_bash from 'prismjs/components/prism-bash'
 
 let toc = [];
 
