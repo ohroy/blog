388dfa946a15a935a888ee15d26682078e11b315
rozbom <75200306@qq.com>
2017-01-04 13:59:52 +0800

    webpack 版本更新

--- webpack.config.js
+++ webpack.config.js
@@ -19,18 +19,18 @@ module.exports = {
         noParse: /es6-promise\.js$/,
         rules: [{
             test: /\.vue$/,
-            loader: 'vue'
+            loader: 'vue-loader'
         },
             {
                 test: /\.js$/,
                 // excluding some local linked packages.
                 // for normal use cases only node_modules is needed.
                 exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
-                loader: 'babel'
+                loader: 'babel-loader'
             },
             {
                 test: /\.css$/,
-                loader: ['style', 'css']
+                loader: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
