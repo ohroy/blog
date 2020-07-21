a31d61d67e8a0ed0f29b8d84bb52c27922271e5c
rozbo <rozbo@live.com>
2020-07-21 19:43:02 +0800

    feat: now support hidden and top

--- src/helpers/github.js
+++ src/helpers/github.js
@@ -1,45 +1,58 @@
-import {
-    EventEmitter
-} from 'events'
-import {config} from '../config'
-import { http } from '../helpers/utils'
-const github = new EventEmitter()
+import { EventEmitter } from "events";
+import { config } from "../config";
+import { http } from "../helpers/utils";
+const github = new EventEmitter();
 let itemsCache = Object.create(null);
-const author=config.user.name;
-const githubURl = '/repos/'+author+'/'+config.user.repo+'/issues';
-let listCache='';
+const author = config.user.name;
+const githubURl = "/repos/" + author + "/" + config.user.repo + "/issues";
+let listCache = "";
+
+github.hasLabel = (issues, name) => {
+  const lables = issues.labels;
+  const now = lables.filter((v) => {
+    return v.name === name;
+  });
+  return now.length > 0;
+};
 
 github.getList = function() {
-    return new Promise((resolve, reject) => {
-        if(listCache!=''){
-            resolve(listCache);
+  return new Promise((resolve, reject) => {
+    if (listCache != "") {
+      resolve(listCache);
+    }
+    http.get(githubURl, { creator: author }).then((response) => {
+      const topTopics = [];
+      const normalTopics = [];
+      response.forEach((v) => {
+        if (github.hasLabel(v, "hidden")) {
+          return;
         }
-        http.get(githubURl,{'creator':author}).then(
-            (response) => {
-                listCache=response;
-                resolve(response);
-            }, reject
-        );
-    });
-}
-
-github.getDetail = id => {
-    return new Promise((resolve, reject) => {
-        if (itemsCache[id]) {
-            console.log("bycache");
-            resolve(itemsCache[id])
-        } else {
-            http.get(githubURl + '/' + id).then(
-                (response) => {
-                    itemsCache[id] = response;
-                    console.log("by get");
-                    resolve(itemsCache[id]);
-                }, reject
-            );
+        if (github.hasLabel(v, "top")) {
+          v.isTop = true;
+          topTopics.push(v);
+          return;
         }
-    });
-}
-
-export {
-    github
+        normalTopics.push(v);
+      });
+      listCache = topTopics.concat(normalTopics);
+      resolve(listCache);
+    }, reject);
+  });
 };
+
+github.getDetail = (id) => {
+  return new Promise((resolve, reject) => {
+    if (itemsCache[id]) {
+      console.log("bycache");
+      resolve(itemsCache[id]);
+    } else {
+      http.get(githubURl + "/" + id).then((response) => {
+        itemsCache[id] = response;
+        console.log("by get");
+        resolve(itemsCache[id]);
+      }, reject);
+    }
+  });
+};
+
+export { github };
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,39 +1,55 @@
 <template>
   <transition name="fade">
-<section class="container-lg px-3" id="features">
-    <div class="blankslate blankslate blankslate-clean-background">
+    <section class="container-lg px-3" id="features">
+      <div class="blankslate blankslate blankslate-clean-background">
         <h3 class="d-inline-block mr-2 mb-1">{{tip}}</h3>
-    </div>
-    <div v-if="loadOk" class="blocks stacked ">
-        <hr class="mt-0 mb-4">
-        <ul class="list-style-none border-left ml-3 pr-4" v-for="item in details" v-bind:key="item.id">
-            <li class="mb-4 clearfix">
-            <div class="float-left d-inline-block border py-1 px-2 ml-n3 mr-2 bg-white" style="height: 32px; width: 32px; border-radius: 50%;">
-                <svg height="16" class="octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock mr-1" aria-label="clock" viewBox="0 0 14 16" version="1.1" width="14" role="img"><path fill-rule="evenodd" d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"></path></svg>
+      </div>
+      <div v-if="loadOk" class="blocks stacked">
+        <hr class="mt-0 mb-4" />
+        <ul
+          class="list-style-none border-left ml-3 pr-4"
+          v-for="item in details"
+          v-bind:key="item.id"
+        >
+          <li class="mb-4 clearfix">
+            <div
+              class="float-left d-inline-block border py-1 px-2 ml-n3 mr-2 bg-white"
+              style="height: 32px; width: 32px; border-radius: 50%;"
+            >
+              <svg
+                height="16"
+                class="octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock octicon octicon-clock mr-1"
+                aria-label="clock"
+                viewBox="0 0 14 16"
+                version="1.1"
+                width="14"
+                role="img"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"
+                />
+              </svg>
             </div>
             <div class="overflow-hidden mt-1 pl-2">
-                {{date_format(item.created_at)}}
-                <h4 class="lh-condensed mt-1">
-                    <router-link :to="'/detail/'+item.number">{{item.title}}</router-link>
-                </h4>
+              {{date_format(item.created_at)}}
+              <h4 class="lh-condensed mt-1">
+                <router-link :to="'/detail/'+item.number">{{item.title}}</router-link>
+              </h4>
             </div>
-        </li>
+          </li>
         </ul>
-
-    </div>
-</section>
+      </div>
+    </section>
   </transition>
 </template>
 
 <script lang="ts">
 import { github } from "../helpers/github";
-
 import { config } from "../config";
-
 import Tip from "../helpers/tip";
-
 import Render from "../helpers/render";
-import {date_format} from '../helpers/utils.js'
+import { date_format } from "../helpers/utils.js";
 export default {
   name: "List",
   data() {
@@ -67,15 +83,15 @@ export default {
     });
   },
   computed: {
-    createAt(){
-      return date_format(this.detail.created_at)
+    createAt() {
+      return date_format(this.detail.created_at);
     },
     details: function() {
       return this.items;
     }
   },
-  methods:{
-    date_format(date: string){
+  methods: {
+    date_format(date: string) {
       return date_format(date);
     }
   }
