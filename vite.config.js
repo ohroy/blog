bc5b12ab144a8768ffd46f393d7572e83fbd2fd0
rozbo <7907969+rozbo@users.noreply.github.com>
2022-02-26 18:17:14 +0800

    refactor: use pnpm and update to newest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import envCompatible from 'vite-plugin-env-compatible';
-import { injectHtml } from 'vite-plugin-html';
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import envCompatible from "vite-plugin-env-compatible";
+import { createHtmlPlugin } from "vite-plugin-html";
 // import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
@@ -11,33 +11,27 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: path.resolve(__dirname,'src')
-      }
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
     ],
-    extensions: [
-      '.mjs',
-      '.js',
-      '.ts',
-      '.jsx',
-      '.tsx',
-      '.json',
-      '.vue'
-    ]
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
   },
   plugins: [
     vue(),
     vueJsx(),
     // viteCommonjs(),
     envCompatible(),
-    injectHtml()
+    createHtmlPlugin({
+      minify: true,
+    }),
   ],
   server: {
-    watch:{
-      usePolling: true
-    }
+    watch: {
+      usePolling: true,
+    },
   },
   build: {
-    rollupOptions: {}
+    rollupOptions: {},
   },
-})
+});
